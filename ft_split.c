e1887654cafa679284cbb3f1bcaa45d867da9db2
Adam Khobba <akhobba@e1r13p6.1337.ma>
2023-11-12 21:27:56 +0100

    12/11/203 21:03

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: akhobba <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 17:04:15 by akhobba           #+#    #+#             */
-/*   Updated: 2023/11/12 18:03:26 by akhobba          ###   ########.fr       */
+/*   Updated: 2023/11/12 21:27:14 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libf.h"
+#include "libft.h"
 
-static int	ft_size(char *s)
+static size_t	ft_size(char const *s, char c)
 {
 	int	i;
 	int	l;	
@@ -23,29 +23,56 @@ static int	ft_size(char *s)
 	{
 		if (s[i] == c)
 			l++;
+		while (s[i] == c)
+			i++;
 		i++;
 	}
 	l++;
 	return (l);
 }
 
-static int ft_strlen_c()
+int	ft_suu(char const *s, char c)
 {
+	int	i;
+	int *irr;
+	int	l;
 
+	i = 0;
+	l = 0;
+	while (s[i])
+	{
+			if (s[i] == c)
+				break ;
+			i++;
+	}
+	return (l);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
 	size_t	i;
+	size_t	l;
 	size_t	size;
 		
 	i = 0;
-	size = ft_size(s) ;
+	l = 0;
+	size = ft_size(s, c) ;
 	arr = (char **)malloc(size + 1);
 	while (i < size)
 	{
-		arr[i] = (char *)malloc ();
+		arr[i] = (char *)malloc (ft_suu(s + l, c));
+		l = ft_suu(s + l, c); 
 	}
 	return (arr);
+}
+
+int main (void)
+{
+	char s[] = "adam khobba ggg";
+	char c = ' '; 
+	int	i;
+
+//	printf("%d",ft_size(s,c));
+		printf("%d",ft_suu(s, c));
 }
