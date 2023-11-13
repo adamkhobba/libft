cbdf6d1c8184d93e13220600d7a4fc44c22bef2a
Adam Khobba <akhobba@e1r13p6.1337.ma>
2023-11-13 18:06:27 +0100

    substr ;)

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: akhobba <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 17:04:15 by akhobba           #+#    #+#             */
-/*   Updated: 2023/11/13 15:47:27 by akhobba          ###   ########.fr       */
+/*   Updated: 2023/11/13 18:06:00 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ static size_t	ft_size(char const *s, char c)
 
 	i = 0;
 	l = 0;
+	while (s[i] == c)
+			i++;
+	i = 0;
 	while(s[i])
 	{
 		if (s[i] == c)
@@ -26,46 +29,70 @@ static size_t	ft_size(char const *s, char c)
 		while (s[i] == c)
 			i++;
 		i++;
-	}
-	l++;
+	} l++;
 	return (l);
 }
-char    **ft_split(char const *s, char c)
-{
-    char    **arr;
-    size_t    i;
-    size_t    j;
-    size_t    l;
-    size_t    size;
 
-    i = 0;
-    l = 0;
-    size = ft_size(s, c) ;
-    arr = (char **)malloc((size + 1) * sizeof(char *));
+int	ft_a(char const *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while(s[i] == c)
+			i++;
+	if (s[i] == '\0')
+		return (0);
+	else
+		return (1);
+}
+
+char	**ft_rspl(char **arr, size_t size, char const *s, char c)
+{
+	size_t i;	
+	size_t j;	
+	size_t l;	
+
+	i = 0;
+	l = 0;
     while (l < size)
     {
       j = 0;
-      while(s[i])
+      while(s[i++])
       {
         if (s[i] == c)
             break  ;
-        i++;
         j++;
       }
-        arr[l] = ft_substr(s,(i - j), j);
-        l++;
+        arr[l++] = ft_substr(s,(i - j), j);
 		while(s[i] == c)
 			i++;
     }
 	arr[l] = NULL;
+	return (arr);
+}
+
+char    **ft_split(char const *s, char c)
+{
+    char    **arr;
+    size_t    size;
+
+    size = ft_size(s, c) ;
+    arr = (char **)malloc((size + 1) * sizeof(char *));
+	if (ft_a(s, c))
+	{
+			arr[0] = NULL;
+			return arr;
+	}
+	arr = ft_rspl(arr, size, s, c);
     return (arr);
 }
 
 int main(void)
 {
-    char const *input = "    ";
+    char const *input = "adam khobba  ";
     char **result = ft_split(input, ' ');
 
+	printf("%zu\n",ft_size(input, ' '));
     // Use the result array as needed, e.g., print the elements
     for (size_t i = 0; result[i] != NULL; i++)
     {
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: akhobba <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 21:50:06 by akhobba           #+#    #+#             */
-/*   Updated: 2023/11/11 11:00:16 by akhobba          ###   ########.fr       */
+/*   Updated: 2023/11/13 18:04:26 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 
 	j = 0;
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
 	if (!s)
-		return (NULL);
-	arr = ft_calloc(1, len + 1);
-	if (!arr)
 		return (NULL);
 	if (ft_strlen(s) <= start)
 	{
+		arr = (char *)malloc(1);
 		arr[0] = '\0';
 		return (arr);
 	}
-	while (s[start] && len > 0)
+	arr = ft_calloc(1, len + 1);
+	if (!arr)
+		return (NULL);
+	while (s[start] && j < len)
 	{
-		arr[j] = s[start];
-		start++;
-		j++;
-		len--;
+		arr[j++] = s[start++];
 	}
 	arr[j] = '\0';
 	return (arr);
