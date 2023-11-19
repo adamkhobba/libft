fbee4585ea262825cd2023cf2aa299a3c9f1d567
Adam Khobba <akhobba@c3r9p9.1337.ma>
2023-11-19 11:04:40 +0100

    list

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,43 +6,50 @@
 /*   By: akhobba <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:19:00 by akhobba           #+#    #+#             */
-/*   Updated: 2023/11/18 21:28:45 by akhobba          ###   ########.fr       */
+/*   Updated: 2023/11/19 09:52:06 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (lst && new)
 	{
 		new->next = *lst;
-		*lst = new;		
+		*lst = new;
 	}
 }
-
+/*
 int main ()
 {
 	t_list	*node1;
-	t_list	*node3;
+	t_list	*node0;
 	int	n;
 	int	n2;
 	int	n1;
 
-	node1 = mallloc(sizeof(t_liste));
-	node->next = malloc (sizeof(t_liste));
+	node1 = malloc(sizeof(t_list));
+	node1->next = malloc (sizeof(t_list));
+	node0 = malloc(sizeof(t_list));
 
 	n = 42; 
-	n = 1337;
+	n1= 1337;
+	n2 = 21;
 
-	node->content = 42;
-	node->next->content = 1337;
-	node->next->next = NULL;
+	node1->content = &n;
+	node1->next->content =&n1;
+	node1->next->next = NULL;
+	node0->content = &n2;
 
-	t_list	current;
-	current = node;
+	ft_lstadd_front(&node1,node0);
+
+	t_list	*current;
+
+	current = node0;
 	while (current)
 	{
-		printf("%d");
+		printf("%d",*(int*)current->content);
+		current = current->next;
 	}
-}
+}*/
